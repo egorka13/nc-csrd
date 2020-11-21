09ff9446cf758c610491235aeaa7303a4f4567d6
Владислав <vlados@MacBook-Pro-Vladislav.local>
2020-11-21 16:59:23 +0300

    realize goToOtherPage

--- client/store/index.js
+++ client/store/index.js
@@ -7,12 +7,32 @@ const state = {
     todo: todoStore.state,
     cpm: cpmStore.state,
     counter: counterStore.state,
-    pages: {
-        cim: 'Customer Info',
-        cpm: 'Customer Problem',
-        roe: 'Order Entry',
-        bim: 'Billing Accounts'
-    }
+    pages: [
+        {
+            minName: 'cim',
+            name: 'Customer Info',
+            fullName: 'Customer Information Management',
+            active: false
+        },
+        {
+            minName: 'cpm',
+            name: 'Customer Problem',
+            fullName: 'Customer Problem Management',
+            active: true
+        },
+        {
+            minName: 'roe',
+            name: 'Order Entry',
+            fullName: 'Residential Order Entry',
+            active: false
+        },
+        {
+            minName: 'bim',
+            name: 'Billing Info',
+            fullName: 'Billing Information Management',
+            active: false
+        },
+    ]
 };
 
 export default new Store({
@@ -20,12 +40,28 @@ export default new Store({
     actions: {
         ...todoStore.actions,
         ...cpmStore.actions,
-        ...counterStore.actions
+        ...counterStore.actions,
+        goToOtherPage(context, payload){
+            let activeListItem = document
+                .querySelector('.sidebar__menu-item._active');
+            activeListItem.classList.toggle('_active');
+            let newListItem = document
+                .querySelector(`.sidebar__menu-item[data-name=${payload}]`);
+            newListItem.classList.toggle('_active');
+
+            context.commit('goToOtherPage', payload);
+        }
     },
     mutations: {
         ...todoStore.mutations,
         ...cpmStore.mutations,
         ...counterStore.mutations,
+        goToOtherPage(state, payload){
+            state.pages.find(page => page.active).active = false;
+            state.pages.find(page => page.minName === payload).active = true;
+            console.log(state);
+            return state;
+        }
     },
     getters: {
         ...todoStore.getters,
